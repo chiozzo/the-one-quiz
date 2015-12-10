1f92fc653780af6fe0503e635ea8d7dc688e673f
Martin Cross <mncross@gmail.com>
2015-12-10 16:19:43 -0600

    Basics completed

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,5 @@
 var Planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"];
+
+document.getElementById('vanilla_output').innerHTML = '<h1>This content generated with vanilla JavaScript</h1>'
+
+$('#jquery_output').html('<h1>This content generated with jQuery</h1>');
