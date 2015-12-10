d0f398f0a014c62930ab73ad2695565c31f69365
Martin Cross <mncross@gmail.com>
2015-12-10 17:35:20 -0600

    Functions and operators completed

--- javascripts/main.js
+++ javascripts/main.js
@@ -10,6 +10,32 @@ $('#jquery_output').html('<h1>This content generated with jQuery</h1>');
 /**
  *Functions and operators
  */
+a = 10;
+b = 3;
+
+var modulus = function(a, b) {
+	console.log("a % b",a % b);
+	return a % b;
+};
+modulus(a, b);
+
+var doMath = function(a, b, callback) {
+	callback(a, b);
+};
+
+var add = function(a, b) {
+	console.log("a + b",a + b);
+	return a + b;
+};
+
+var subtract = function(a, b) {
+	console.log("a - b",a - b);
+	return a - b;
+};
+
+doMath(a, b, add);
+
+doMath(a, b, subtract);
 
 
 
